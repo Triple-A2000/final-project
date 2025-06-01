66bdab3ed17ef587926985106c865aab99e36501
Ayomide Boye <boyeayomide@gmail.com>
2025-06-01 13:33:27 -0500

    second commit-updating the cart to add quantity instead of dupli

--- src/context/CartContext.js
+++ src/context/CartContext.js
@@ -1,4 +1,4 @@
-import { createContext, useState, useContext } from "react";
+import { createContext, useContext, useState } from "react";
 
 const CartContext = createContext();
 
@@ -6,15 +6,41 @@ export const CartProvider = ({ children }) => {
   const [cart, setCart] = useState([]);
 
   const addToCart = (product) => {
-    setCart(prev => [...prev, product]);
+    setCart(prev => {
+      const existingItem = prev.find(item => item.id === product.id);
+      if (existingItem) {
+        // Update quantity
+        return prev.map(item =>
+          item.id === product.id
+            ? { ...item, quantity: item.quantity + 1 }
+            : item
+        );
+      } else {
+        // Add new item with quantity 1
+        return [...prev, { ...product, quantity: 1 }];
+      }
+    });
   };
 
   const removeFromCart = (id) => {
-    setCart(prev => prev.filter(item => item.id !== id));
+    setCart(prev =>
+      prev
+        .map(item =>
+          item.id === id
+            ? { ...item, quantity: item.quantity - 1 }
+            : item
+        )
+        .filter(item => item.quantity > 0)
+    );
+  };
+
+  const getQuantity = (id) => {
+    const item = cart.find(item => item.id === id);
+    return item ? item.quantity : 0;
   };
 
   return (
-    <CartContext.Provider value={{ cart, addToCart, removeFromCart }}>
+    <CartContext.Provider value={{ cart, addToCart, removeFromCart, getQuantity }}>
       {children}
     </CartContext.Provider>
   );
