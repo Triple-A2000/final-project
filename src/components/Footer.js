c2683cae452fa65af46e62bd08432d73250a4fc9
Connor Hall <connrhall204@gmail.com>
2025-06-02 10:07:56 -0500

    changed the title

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -2,7 +2,7 @@ function Footer() {
   return (
     <footer>
       <div className="container center">
-        <p>&copy; 2025 React Store. All rights reserved.</p>
+        <p>&copy; 2025 Oasis. All rights reserved.</p>
       </div>
     </footer>
   );
