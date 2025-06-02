74cd5e02da30d152d565b24d62338c9345658eae
Ayomide Boye <boyeayomide@gmail.com>
2025-06-01 22:03:35 -0500

    not-found-page js code added

--- src/pages/NotFound.js
+++ src/pages/NotFound.js
@@ -1,7 +1,14 @@
+import { Link } from "react-router-dom";
+import "../css/index.css";
+
 function NotFound() {
   return (
-    <></>
-  )
+    <section className="notfound-container">
+      <h1>404</h1>
+      <p>Oops! Page not found.</p>
+      <Link to="/" className="home-link">Go back to Home</Link>
+    </section>
+  );
 }
 
 export default NotFound;
