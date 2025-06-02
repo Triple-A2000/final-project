15d8de5c2faab6159a75e569ca90f5f12b275244
Connor Hall <connrhall204@gmail.com>
2025-06-01 23:53:38 -0500

    fixed an annoying issue

--- src/index.js
+++ src/index.js
@@ -2,7 +2,10 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './css/index.css';
 import App from './App';
-import { BrowserRouter as Router } from 'react-router-dom';
+import { HashRouter as Router } from 'react-router-dom';
+// We were having issues with the homepage not loading on startup for most of the project.
+// While I don't know how this works exactly it was the easiest fix I could find at 11:50pm
+// on the night before submission. -Connor Hall
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -12,4 +15,3 @@ root.render(
     </Router>
   </React.StrictMode>
 );
-
