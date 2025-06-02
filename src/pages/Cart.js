69326ca22d4dc3ac0b2ca02461c626845b836597
Ayomide Boye <boyeayomide@gmail.com>
2025-06-01 19:01:07 -0500

    styled it

--- src/pages/Cart.js
+++ src/pages/Cart.js
@@ -1,42 +1,63 @@
+import "../css/index.css";
+
 const Cart = ({ cart, addToCart, removeFromCart }) => {
   const getItemTotal = (item) => item.price * item.quantity;
 
   const getCartTotal = () => {
-    return cart.reduce((total, item) => total + getItemTotal(item), 0).toFixed(2);
+    return cart
+      .reduce((total, item) => total + getItemTotal(item), 0)
+      .toFixed(2);
   };
 
   return (
-    <section>
-      <h2>Your Cart</h2>
+    <section className="cart-container">
+      <h2 className="cart-header">Cart</h2>
 
       {cart.length === 0 ? (
         <p>Your cart is empty.</p>
       ) : (
-        <div>
+        <>
           {cart.map((item) => (
-            <div key={item.id}>
-              <div>
+            <div className="cart-item" key={item.id}>
+              <div className="cart-item-details">
                 <img src={item.image} alt={item.title} />
-                <div>
+                <div className="cart-item-info">
                   <h3>{item.title}</h3>
-                  <p>Price: ${item.price.toFixed(2)}</p>
-                  <p>Quantity: {item.quantity}</p>
+                  <p>${item.price.toFixed(2)}</p>
+                  <div className="quantity-controls">
+                    <button
+                      className="quantity-btn"
+                      onClick={() => removeFromCart(item.id)}
+                    >
+                      -
+                    </button>
+                    <span className="quantity-value">{item.quantity}</span>
+                    <button
+                      className="quantity-btn"
+                      onClick={() => addToCart(item)}
+                    >
+                      +
+                    </button>
+                  </div>
                   <p>Total: ${getItemTotal(item).toFixed(2)}</p>
                 </div>
               </div>
-
               <div>
-                <button onClick={() => removeFromCart(item.id)}>-</button>
-                <button onClick={() => addToCart(item)}>+</button>
+                <button
+                  className="remove-btn"
+                  onClick={() => removeFromCart(item.id)}
+                >
+                  Remove
+                </button>
               </div>
             </div>
           ))}
 
-          <div>
-            <p>Total Price: ${getCartTotal()}</p>
-            <button>Proceed to Checkout</button>
+          <div className="cart-total-line">
+            <p>Total: ${getCartTotal()}</p>
+            <button className="checkout-btn">Checkout</button>
           </div>
-        </div>
+        </>
       )}
     </section>
   );
